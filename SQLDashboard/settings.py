7648610e2c81e2b11ac2a1d8a893cb13f2bdd5fe
Marcos Freccia <marcos.freccia@zalando.de>
2018-12-24 18:35:00 +0100

    Reading settings.cfg dynamically

--- SQLDashboard/settings.py
+++ SQLDashboard/settings.py
@@ -13,11 +13,12 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 import configparser
 
-config = configparser.ConfigParser()
-config.read(r'C:\Users\mfreccia\Documents\Private\GitHub\SQLDashboard\settings.cfg')
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+config = configparser.ConfigParser()
+config.read(os.path.join(BASE_DIR, "settings.cfg"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
